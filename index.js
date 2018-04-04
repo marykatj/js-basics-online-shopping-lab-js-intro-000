4bffdee5716ad181aae63d1348dabd4d408fb97a
Mary-Kate James <marykatj@gmail.com>
2018-04-04 16:25:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  if (cart.length === 0) {
+  if (cart.length < 1) {
     return "Your shopping cart is empty."
   } else {
       for (let i = 0; i < cart.length; i++) {

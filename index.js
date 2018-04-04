bf9cf15e0c2291e40d3166f6938aaf8f09189920
Mary-Kate James <marykatj@gmail.com>
2018-04-04 16:37:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,9 +20,27 @@ function viewCart() {
 
   if (cart.length < 1) {
     return console.log("Your shopping cart is empty.")
-  } else {
-      for (let i = 0; i < cart.length; i++) {
-        return `In your cart, you have ${item[i]} at ${itemPrice[i]}.`
+  } 
+
+  var itemsAndPrices = [];
+  
+  for (let i = 0; i < cart.length; i++) {
+      var itemAndPrice = cart[i];
+      var item = Object.keys(itemAndPrice)[0];
+      let price = itemAndPrice[item];
+
+      itemsAndPrices.push(`${item} at \$${price}`);
     }
-  }
+
+    switch(itemsAndPrices.length) {
+      case 1: 
+        break;
+      case 2: 
+        itemsAndPrices = itemsAndPrices.join(" and ");
+        break;
+      default:
+        itemsAndPrices[1-1] = "and ".concat(itemsAndPrices[1-1]);
+        itemsAndPrices = itemsAndPrices.join(", ");
+    }
+    console.log(`In your cart, you have ${itemsAndPrices}.`);
 }

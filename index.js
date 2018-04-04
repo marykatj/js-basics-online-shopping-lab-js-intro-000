153a8ddd4e1f48907799c6a4ae0cca6f071ed430
Mary-Kate James <marykatj@gmail.com>
2018-04-04 16:16:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 var cart = [];
 
+function getCart(){
+    return cart;
+}
+
+function setCart(c) {
+  cart = c;
+  return cart;
+}
+
 function addToCart(item) {
-  var newObject = Object.Assign({}, object, {[key] : value});
+  var itemName = name
+  var itemPrice = Math.floor(Math.Random() * 100) + 1;
+  var newObject = Object.Assign({}, cart);
+  newObject.push({[itemName] : itemPrice});
   return newObject;
 }

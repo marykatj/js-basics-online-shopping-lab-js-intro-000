5c15a545057ef156ce7dde4a18a32fec869f5cc9
Mary-Kate James <marykatj@gmail.com>
2018-04-04 16:19:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  var itemPrice = Math.floor(Math.Random() * 100) + 1;
+  var itemPrice = Math.floor(Math.random() * 100) + 1;
   cart.push({ [item] : itemPrice });
   console.log(`${item} has been added to your cart.`);
   return cart;
